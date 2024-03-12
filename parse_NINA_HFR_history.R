69530c940d85d9e93fad68c8b274ba3372237026
Alexander Reinartz <areinartz89@gmail.com>
2024-03-13 00:37:35 +0100

    added plotting

--- parse_NINA_HFR_history.R
+++ parse_NINA_HFR_history.R
@@ -8,7 +8,7 @@ library(ggplot2)
 #### script to parse and plot NINA HFR history file
 
 
-hfrhist_file <- "~/N.I.N.A/2023-09-23_history.csv"
+hfrhist_file <- "~/N.I.N.A/2023-12-17_history.csv"
 
 hfrhist <- read.csv(hfrhist_file)
 
@@ -40,3 +40,26 @@ ggplot(molten_QC[molten_QC$variable %in% c("HFR","Stars"),], aes(x=Filename, y=v
 
 
 ordr <- QC_metrics[order(QC_metrics$Stars),]
+
+
+##### move
+
+
+
+ggplot(molten_QC[molten_QC$variable %in% c("Stars"),], aes(x=Filename, y=value, group=variable)) +
+  geom_line() +
+  geom_hline(yintercept = 230) +
+  facet_grid(rows = vars(variable), scales = "free") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
+
+table(QC_metrics$Stars < 230)
+
+a <- QC_metrics[QC_metrics$Stars < 230,1:3]
+a <- a[order(a$Filename),]
+a <- a[33:nrow(a),]
+
+
+
+
+
